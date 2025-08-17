3e796ccd8d0887abdeeb8f36675e48a69a13a887
Chirag21-dev <chiragks21@outlook.com>
2025-08-17 23:37:39 +0530

    Create pub_subnet.py

---
+++ pub_subnet.py
@@ -0,0 +1,14 @@
+import boto3
+
+# Initialize a session using Amazon EC2
+ec2 = boto3.resource('ec2')
+client = boto3.client('ec2')
+
+# Create a VPC
+vpc = ec2.create_vpc(CidrBlock='10.0.0.0/16')
+vpc.wait_until_available()
+print(f'Created VPC: {vpc.id}')
+
+
+public_subnet = ec2.create_subnet(CidrBlock='10.0.1.0/24', VpcId=vpc.id, AvailabilityZone='ap-south-1a')
+print(f'Created Public Subnet: {public_subnet.id}')
