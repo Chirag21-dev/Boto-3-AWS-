43b793e60be1a83aa6e210e4b146ee666c5eecc0
Chirag21-dev <chiragks21@outlook.com>
2025-08-19 15:10:43 +0530

    Create dict_set.py

---
+++ dict_set.py
@@ -0,0 +1,17 @@
+# Dictionary
+dict_var = {'a': 1, 'b': 2, 'c': 3}
+print("Dictionary Variable:", dict_var)
+
+# Set
+set_var = {1, 2, 3, 4, 5}
+print("Set Variable:", set_var)
+
+# Dictionary operations
+dict_var['d'] = 4
+del dict_var['a']
+print("Modified Dictionary:", dict_var)
+
+# Set operations
+set_var.add(6)
+set_var.remove(3)
+print("Modified Set:", set_var)
