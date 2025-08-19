57a22120de4213a5322dc5edbfdcdcac317ff77f
Chirag21-dev <chiragks21@outlook.com>
2025-08-19 17:55:56 +0530

    Added the scripts related to networking.

---
+++ networking/ICMP.py
@@ -0,0 +1,13 @@
+from scapy.all import *
+
+# Create an ICMP packet
+packet = IP(dst="www.google.com") / ICMP()
+
+# Send the packet and receive a response
+response = sr1(packet, timeout=2, verbose=False)
+
+# Check if a response was received
+if response:
+    print(f"Received response from {response.src}")
+else:
+    print("No response received")
